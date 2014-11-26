e1b486e8a1124b5ca53f62a24699a2053b7aaac1
Mel Quark <mel.quark@gmail.com>
2014-11-25 23:57:15 -0800

    add livingston's exercise

--- lab1.js
+++ lab1.js
@@ -20,7 +20,7 @@
 */
 
 /* ----------------- Helper ------------------------------------------------
- I've implemented a function that will help you sanity-check your code.
+ We've implemented a function that will help you sanity-check your code.
 */
 
 function assert(expression, failureMessage) {
@@ -44,31 +44,32 @@ assert(1===1);
 assert(1===2, "this is an assertion failure example. 1===2");
 
 /*
- TODO: 4 points
- Use assert twice. Use any boolean expressions that you like, but preferably
+ TODO: 8 points
+ Invoke assert twice. Use any boolean expressions that you like, but preferably
  zoo-themed.  Make one pass and one fail. In the failure message, describe why
- it failed. Each example is worth 2 points.
+ it failed.
 */
 
 /* ----------------- Meerkats -------------------------------------------------
- Meerkats make a sort of chirping noise (according to my 30 seconds of research)
- We're going to translate two sentences into meerkat speech.
+ Meerkats make a sort of chirping noise (according to my 30 seconds of
+ research).  We're going to translate two sentences into meerkat speech.
 */
 
 var sentence1 = "More food please."
 var sentence2 = "Come over here so you can scratch my belly."
 
 /*
- TODO: 10 points
- Your goal is to replace the words in the above sentences with "chirp"
- The assertions at the end of this section should pass.
- Use two different kinds of loops to implement this.
+ TODO: 20 points
+ Your goal is to replace the words in the above sentences with "chirp" The
+ assertions at the end of this section should pass when you're done.  Use
+ **two** different kinds of loops to implement this.
  HINT: the "split" method on String will be useful.
 */
 
 assert(sentence1 === "chirp chirp chirp.", "sentence 1 should have 3 chirps");
 assert(sentence2 === "chirp chirp chirp chirp chirp chirp chirp chirp chirp.",
   "sentence 2 should have 9 chirps");
+
 
 /* ----------------- Favorite Animals ----------------------------------------
  The zoo is closing in 20 minutes. You still haven't seen your four favorite
@@ -80,10 +81,45 @@ assert(sentence2 === "chirp chirp chirp chirp chirp chirp chirp chirp chirp.",
 var favoriteAnimals = ["elephant", "penguin", "eagle", "camel"];
 var nextAnimal;
 
-// TODO: 8 points
+// TODO: 10 points
 // Assign one of your favorite animals to nextAnimal using Math.random() to pick
 
 assert(nextAnimal, "assign something to nextAnimal");
+
+/* ----------------- Survey Says...----------------------------------------
+ Like any conscientious business, the Woodland Park Zoo collects statistics
+ about its animal exhibits. The animalExhibitsStats object is a summary of what
+ they found.
+*/
+
+// TODO: 4 points
+// All of the property names below are surrounded by quotes. Some of the names
+// don't require quotes for the code to work. Remove the unnecessary quotes.
+
+var animalExhibitStats = {
+  "numberOpen": 13,
+  "number closed": 2,
+  "petting-zoo-open": true,
+  "mostPopular": "Lucky the Emperor Penguin",
+  "2ndMostPopular": "Dumbo the Depressed Donkey"
+}
+
+/*
+ TODO: 4 points
+ The animalExhibitStats object is indexed four times below using bracket
+ notation. WITHOUT changing any of the property names, replace the bracket
+ notation with Dot notation wherever possible.
+*/
+
+assert(animalExhibitStats["numberOpen"] === 13, "there should be 13 open exhibits");
+assert(animalExhibitStats["number closed"] === 2, "there should be 2 closed exhibits");
+assert(animalExhibitStats["petting-zoo-open"], "hey! =( i was promised meerkats!");
+assert(animalExhibitStats["2ndMostPopular"] === "Dumbo the Depressed Donkey",
+  "Dumbo is supposed to be second place, as usual");
+
+// TODO: 4 points
+// Write an assert about the "mostPopular" property of animalExibitStats.
+// Use the above examples to guide you.
 
 /* ----------------- Code Style ----------------------------------------
  TODO: 10 points
@@ -98,5 +134,5 @@ assert(nextAnimal, "assign something to nextAnimal");
   ./node_modules/.bin/grunt jscs
 
  Error and warning descriptions will be printed in the terminal.
- To get full points, correct all of the errors. 
+ To get full points, correct all of the errors/warnings.
 */
